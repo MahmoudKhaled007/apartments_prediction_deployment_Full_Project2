342d25f6a3adb2b8bfd5032ae7255d2307f240b7
karim tarek <58817281+karimtahmed@users.noreply.github.com>
2022-04-30 04:34:03 +0200

    ..

--- Web App Deployment/app.py
+++ Web App Deployment/app.py
@@ -18,28 +18,41 @@ def index():
 @app.route('/predict', methods=['GET'])
 def predict():
     all_data = request.args
-    temp = float(all_data['temp'])
-    humidity = float(all_data['humidity'])
-    hour = int(all_data['datetime'].split('T')[1].split(':')[0])
-    is_rush_hour = int(all_data['is_rush_hour'])
-    location = location_dummies[all_data['location']]
-    compound = compound_dummies[all_data['compound']]
-    bedrooms = bedrooms_dummies[all_data['bedrooms']]
-    level = level_dummies[all_data['level']]
-    bathrooms = bathrooms_dummies[all_data['bathrooms']]
-    payment_option = payment_option_dummies[all_data['payment_option']]
-    delivery_term = delivery_term_dummies[all_data['delivery_term']]
-    delivery_date = delivery_date_dummies[all_data['delivery_date']]
-    price_type=price_type_dummies[all_data['price_type']]
-    month=month_dummies[all_data['month']]
-    x = [temp, humidity, hour, is_rush_hour]
+    area = int(all_data['Area'])
+    furnished = int(all_data['Datetime'])
+    down_payment = int(all_data['Down_payment'])
+    electricity_meter = int(all_data['Electricity_Meter'])
+    balcony = int(all_data['Balcony'])
+    water_meter = int(all_data['Water_Meter'])
+    elevator = int(all_data['Elevator'])
+    security = int(all_data['Security'])
+    landline = int(all_data['Landline'])
+    pets_allowed = int(all_data['Pets_Allowed'])
+    covered_parking = int(all_data['Covered_Parking'])
+    private_garden = int(all_data['Private_Garden'])
+    natural_gas = int(all_data['Natural_Gas'])
+    pool = int(all_data['Pool'])
+    maids_room = int(all_data['Maids_Room'])
+    central_ac_heating = int(all_data['Central_Ac_Heating'])
+    built_in_kitchen_appliances = int(all_data['Built_In_Kitchen_Appliances'])
+    location = location_dummies[all_data['Location']]
+    compound = compound_dummies[all_data['Compound']]
+    bedrooms = bedrooms_dummies[all_data['Bedrooms']]
+    level = level_dummies[all_data['Level']]
+    bathrooms = bathrooms_dummies[all_data['Bathrooms']]
+    payment_option = payment_option_dummies[all_data['Payment_Option']]
+    delivery_term = delivery_term_dummies[all_data['Delivery_Term']]
+    delivery_date = delivery_date_dummies[all_data['Delivery_Date']]
+    price_type=price_type_dummies[all_data['Price_Type']]
+    month=month_dummies[all_data['Month']]
+    x = [price , area , furnished , down_payment , electricity_meter , balcony , water_meter , elevator , security , natural_gas , landline , pets_allowed , covered_parking , private_garden , pool , maids_room , central_ac_heating , built_in_kitchen_appliances]
     x += location + compound + bedrooms + month + level + bathrooms + payment_option + delivery_term + delivery_date + price_type, month
 
 
     x = scaler.transform([x])
-    bikes_count = round(model.predict(x)[0])
-
-    return render_template('prediction.html', bikes_count=bikes_count)
+    meterprice = round(model.predict(x)[0])
+    price=meterprice*area
+    return render_template('prediction.html', meterprice=meterprice)
 
 
 
